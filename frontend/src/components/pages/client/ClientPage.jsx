35d344fc3a7b1f0f1c0c3be764a029f3b4dc6591
adamdon <mail@adamdon.co.uk>
2023-10-11 16:01:00 +0100

    Added client page update

--- frontend/src/components/pages/client/ClientPage.jsx
+++ frontend/src/components/pages/client/ClientPage.jsx
@@ -1,22 +1,122 @@
 import ContentContainer from "../../layout/ContentContainer.jsx";
+import {useData} from "../../../utilities/DataContextProvider.jsx";
+import {useState} from "react";
+import {toast} from "react-toastify";
 
 
 
 
 export default function ClientPage() {
 
+
+  const [data, setData] = useData();
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  // const [ref, setRef] = useState("");
+  // const [text, setText] = useState("");
+
+
+
+  async function checkinOnClick()
+  {
+    setData({isDisabled: true});
+    setIsLoading(true);
+    const toastId = toast.loading('Checking in...');
+
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // small sleep for ux drama
+      // const returnedItem = await fetchCheckin();
+      toast.success('checked-In');
+      console.info("create - returnedItem: ");
+    }
+    catch (error){
+      console.error(error);
+      toast.error(error.message);
+    }
+
+    toast.dismiss(toastId);
+    setIsLoading(false);
+    setData({isDisabled: false});
+  }
+
+
+  async function checkOutOnClick()
+  {
+    setData({isDisabled: true});
+    setIsLoading(true);
+    const toastId = toast.loading('Checking in...');
+
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // small sleep for ux drama
+      // const returnedItem = await fetchCheckin();
+      toast.success('checked-In');
+      console.info("create - returnedItem: ");
+    }
+    catch (error){
+      console.error(error);
+      toast.error(error.message);
+    }
+
+    toast.dismiss(toastId)
+    setIsLoading(false);
+    setData({isDisabled: false});
+  }
+
+
+  async function fetchCheckin(newRef, newText)
+  {
+    try {
+      let newItem = {
+        "ref": newRef,
+        "name": newText
+      }
+
+      let requestBody = JSON.stringify(newItem);
+      let methodType = "POST"
+      let requestUrl = data.backendUrl + "/api/check-in";
+      let requestHeaders = {"Content-Type": "application/json"};
+      let requestOptions = {method: methodType, headers: requestHeaders, body: requestBody};
+
+      const response = await fetch(requestUrl, requestOptions);
+      const jsonData = await response.json();
+
+      return jsonData
+    }
+    catch (error) {
+      throw error;
+    }
+  }
+
+
   return (
     <>
       <ContentContainer>
-        <div className="hero bg-base-200 rounded">
-          <div className="hero-content text-center">
-            <div className="w-100">
 
-              <h1>Client page here</h1>
+        <h1>Client page here</h1>
 
-            </div>
-          </div>
-        </div>
+        <button className="btn btn-block btn-outline btn-accent" onClick={checkinOnClick} disabled={data.isDisabled}>
+          <span>Check in</span>
+          {isLoading ? <span className="loading loading-spinner"></span> : <></> }
+        </button>
+
+        <button className="btn btn-block btn-outline btn-accent" onClick={checkOutOnClick} disabled={data.isDisabled} style={{height: 40}}>
+          <span>Check Out</span>
+          {isLoading ? <span className="loading loading-spinner"></span> : <></> }
+        </button>
+
+        {/*<div className="indicator">*/}
+        {/*  <span className="indicator-item badge badge-secondary">In</span>*/}
+        {/*  <button onClick={checkinOnClick} className="btn btn-block" disabled={data.isDisabled}>Check in</button>*/}
+        {/*</div>*/}
+
+        {/*<div className="indicator">*/}
+        {/*  <span className="indicator-item badge badge-secondary">Out</span>*/}
+        {/*  <button onClick={checkinOnClick} className="btn btn-block" disabled={data.isDisabled}>Check Out</button>*/}
+        {/*</div>*/}
+
+
+
       </ContentContainer>
     </>
   )
